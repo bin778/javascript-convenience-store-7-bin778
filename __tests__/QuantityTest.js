8259124f3fa1f8e23382e5e9f7ce84af43a283b3
최영빈 <bin778@naver.com>
2024-11-07 00:04:07 +0900

    test: 구매한 상품이 재고 목록에 있는지 테스트

--- __tests__/QuantityTest.js
+++ __tests__/QuantityTest.js
@@ -13,4 +13,11 @@ describe('구매할 상품과 수량을 입력받는 테스트', () => {
       ValidatePurchaseProducts.validateInputFormat(input);
     }).toThrow(ERROR_MESSAGE.PURCHASE.inputFormat);
   });
+
+  test.each([[[{ product: '콜라' }]], [[{ product: '오렌지쥬스' }]]])('재고에 없는 상품인 경우', (purchaseProducts) => {
+    const products = { 오렌지주스: [{ name: '오렌지주스' }] };
+    expect(() => {
+      ValidatePurchaseProducts.validateNonProducts(purchaseProducts, products);
+    }).toThrow(ERROR_MESSAGE.PURCHASE.nonProducts);
+  });
 });
