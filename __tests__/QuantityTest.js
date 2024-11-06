3bd477d7bb404c1d9aabadbe975e258caebc22c0
최영빈 <bin778@naver.com>
2024-11-06 22:33:53 +0900

    test: 상품 및 수량 입력 형식 테스트

---
+++ __tests__/QuantityTest.js
@@ -0,0 +1,16 @@
+import ValidateQuantity from '../src/validation/ValidateQuantity.js';
+import { ERROR_MESSAGE } from '../src/constant/Error.js';
+
+describe('구매할 상품과 수량을 입력받는 테스트', () => {
+  test.each([
+    [['콜라-10]', '사이다-3]']],
+    [['[사이다-1]', '[사이다-2]', '[오렌지쥬스10]']],
+    [['[콜라-10][사이다-3]']],
+    [['(오렌지쥬스-10)']],
+    [['[콜라+8]']],
+  ])('입력 형식이 맞지 않는 경우', (input) => {
+    expect(() => {
+      ValidateQuantity.validateInputFormat(input);
+    }).toThrow(ERROR_MESSAGE.QUANTITY.inputFormat);
+  });
+});
