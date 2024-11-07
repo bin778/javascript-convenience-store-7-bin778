ca6d8a920fa649f048be5a657b56be0a6ad3acb5
최영빈 <bin778@naver.com>
2024-11-07 23:24:22 +0900

    feat(PromotionPrice): 프로모션 할인 추가

--- src/model/PromotionPrice.js
+++ src/model/PromotionPrice.js
@@ -6,8 +6,17 @@ class PromotionPrice {
   }
 
   static setPromotionPrice(name, promotion, quantity, productsList, promotionsList) {
-    // [ ] 프로모션은 N개 구매(1+1, 2+1) 작성
-    console.log(name, promotion, quantity);
+    const promotionPrice = this.calculatePromotionPrice(name, promotion, quantity, productsList, promotionsList);
+    return promotionPrice;
+  }
+
+  static calculatePromotionPrice(name, promotion, quantity, productsList, promotionsList) {
+    const product = productsList[name]?.find((product) => product.promotion === promotion);
+    const promoDetails = promotionsList[promotion][0];
+    const { buy, get } = promoDetails;
+    const promoUnits = Math.floor(quantity / (buy + get));
+    const totalGetUnits = promoUnits * get;
+    return totalGetUnits * product.price;
   }
 }
 
